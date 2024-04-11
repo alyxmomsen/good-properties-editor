a28e80cb7aa0b9e4c87bd0a40685bc37c853deee
alex <shizofranky@gmail.com>
2024-04-11 10:56:02 +0300

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+interface Param {
+
+  id: number;
+  
+  name: string;
+  
+  type: 'string';
+  
+  }
+
+  interface ParamValue {
+
+    paramId: number;
+    
+    value: string;
+    
+    }
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
