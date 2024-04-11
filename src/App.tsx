32c5347df24448d38fbe534d8113a736d96ffcc2
alex <shizofranky@gmail.com>
2024-04-11 11:33:05 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+interface Color {}
 
 interface Param {
-
   id: number;
-  
+
   name: string;
-  
-  type: 'string';
-  
-  }
 
-  interface ParamValue {
+  type: "string";
+}
 
-    paramId: number;
-    
-    value: string;
-    
-    }
+interface ParamValue {
+  paramId: number;
 
+  value: string;
+}
 
+interface Model {
+  paramValues: ParamValue[];
 
+  colors: Color[];
+}
+
+interface Props {
+  params: Param[];
+
+  model: Model;
+}
+
+/* class ParamEditor extends React.Component<Props, State> {
+          public getModel(): Model {
+          
+          }
+          
+          } */
+
+const ModelEditor: React.FC<{ params: Param[] }> = ({ params }) => {
+  return (
+    <div>
+      <h1>Model Editor</h1>
+    </div>
+  );
+};
+
+const params_example: Param[] = [
+  {
+    id: 1,
+    name: "color",
+    type: "string",
+  },
+  {
+    id: 2,
+    name: "length",
+    type: "string",
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      
+      <ModelEditor params={params_example} />
     </div>
   );
 }
